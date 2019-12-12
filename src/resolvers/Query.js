ad25009c4c37d2ef3de7e239fdf1b9323dade2d1
Saiteja-vadlapatla <saitejavadlapatla@gmail.com>
2019-12-12 11:42:44 +0530

    Items mutations and query with graphql db

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,7 +1,13 @@
+const { forwardTo } = require("prisma-binding");
+
 const Query = {
-    dogs(parent, args, ctx, info) {
-        return global.dogs;
-    }
+    // dogs(parent, args, ctx, info) {
+    //     return global.dogs;
+    // }
+    // items(parent, args, ctx, info) {
+    //     return ctx.db.query.items();
+    // }
+    items: forwardTo('db')
 };
 
 module.exports = Query;
